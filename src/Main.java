bc4bfa2b6e62f96382d3c7a981436babec9e953b
karti <kartikkumarji24@gmail.com>
2024-10-08 00:12:22 +0530

    suraj now you understand

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Suraj guptaaaaa");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
 
             System.out.println("suraj's house " + i);
 
